a0aa9988b60b67fa4e8353ab2cc6d7fed1c78efc
Crisarchero <YOUNGERBROWN123@GMAIL.COM>
2023-02-10 12:22:03 -0600

    Initial commit

---
+++ src/Navbar.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <nav>
+        <a href = "/">Our Song</a>
+        <ul>
+            <li>
+
+            </li>
+        </ul>
+    </nav>
+  )
+}
+
+export default Navbar
